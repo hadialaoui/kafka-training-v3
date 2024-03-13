8a1461666947b36687117af5167e1494d8ecd08d
abdelhakim <a.hadialaoui@gmail.com>
2024-03-13 15:51:03 +0000

    Init project and add producer demo

---
+++ kafka-basics/src/main/java/DemoProducer.java
@@ -0,0 +1,33 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class DemoProducer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(DemoProducer.class.getSimpleName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Start Kafka producer");
+        // Create producer properties
+        Properties  properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // Create kafka producer
+        var producer = new KafkaProducer<String, String>(properties);
+        // Create producer Record
+        var producerRecord = new ProducerRecord<String, String>("demo-topic", "hello world");
+        // Send data
+        producer.send(producerRecord);
+
+        // Flush and close producer
+        producer.close();
+
+    }
+}
